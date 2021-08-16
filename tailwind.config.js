240426ffbd5082281f5d0bdd543247b4bee675d2
ConnorMarble <68395737+ConnorMarble@users.noreply.github.com>
2021-08-15 21:39:31 -0400

    Added tailwind plugin and .env

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,20 @@
+/* eslint-disable global-require */
 module.exports = {
   mode: "jit",
-  purge: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
+  purge: ["./src/**/*.{js,ts,jsx,tsx}"],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        amazon_blue: {
+          light: "#232f3e",
+          DEFAULT: "#131921",
+        },
+      },
+    },
   },
   variants: {
     extend: {},
   },
-  plugins: [],
+  plugins: [require("@tailwindcss/line-clamp")],
 };
